20d7050000c436de0afc27032482ac1b13f67e05
hmwong <hmwong247@gmail.com>
2025-03-19 00:10:59 +0800

    added functions and fixed null pointer dereferencing

--- core/room/playlist.go
+++ core/room/playlist.go
@@ -81,12 +81,59 @@ func (playlist *Playlist) Remove(id int) error {
 	return errors.New("id not found")
 }
 
-// return a copy of the music node
+// linkedlist has no `move()` implementation, this is a workaround method
+// this moves the target node to before the other node
+func (playlist *Playlist) Move(id, other int) error {
+	// search the node, ensure they exists
+	playlist.RLock()
+	defer playlist.RUnlock()
+	var n1 *linkedlist.Node[*MusicInfo] = nil
+	for n1 = playlist.list.Head(); n1 != nil; n1 = n1.Next() {
+		if infoPtr := n1.Val(); (*infoPtr).ID == id {
+			break
+		}
+	}
+	if n1 == nil {
+		errStr := fmt.Sprintf("node not found, id: %v", id)
+		return errors.New(errStr)
+	}
+	var n2 *linkedlist.Node[*MusicInfo] = nil
+	for n2 = playlist.list.Head(); n2 != nil; n2 = n2.Next() {
+		if infoPtr := n2.Val(); (*infoPtr).ID == other {
+			break
+		}
+	}
+	if n2 == nil {
+		errStr := fmt.Sprintf("node not found, other: %v", other)
+		return errors.New(errStr)
+	}
+
+	// create a dummy node and swap them, then remove the dummy node
+	info := &MusicInfo{}
+	if err := playlist.list.InsertBefore(&info, n2); err != nil {
+		return err
+	}
+	dummy := n2.Prev() // reference the node for later clean up
+	if err := playlist.list.Swap(n1, dummy); err != nil {
+		return err
+	}
+	if err := playlist.list.Remove(dummy); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (playlist *Playlist) Dequeue() (*MusicInfo, error) {
 	playlist.Lock()
 	defer playlist.Unlock()
 
 	node := playlist.list.Head()
+	if node == nil {
+		err := fmt.Errorf("Trying to dequeue an empty playlist")
+		return nil, err
+	}
+
 	info := node.Val()
 	if err := playlist.list.Remove(node); err != nil {
 		return nil, err
@@ -95,11 +142,31 @@ func (playlist *Playlist) Dequeue() (*MusicInfo, error) {
 	return *info, nil
 }
 
+func (playlist *Playlist) Head() *MusicInfo {
+	playlist.RLock()
+	defer playlist.RUnlock()
+
+	node := playlist.list.Head()
+	if node == nil {
+		return nil
+	}
+	info := node.Val()
+
+	return *info
+}
+
 func (playlist *Playlist) Clear() {
 	playlist.Lock()
 	defer playlist.Unlock()
 
 	playlist.list.Init()
+}
+
+func (playlist *Playlist) Size() int {
+	playlist.Lock()
+	defer playlist.Unlock()
+
+	return playlist.list.Size()
 }
 
 func (playlist *Playlist) Traverse() {
